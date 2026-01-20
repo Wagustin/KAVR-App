2cd9cf71a64117378dbf8ae60dab53fd497f61a4
wagustin <venturasaldanaa@gmail.com>
2026-01-19 19:08:37 -0500

    actu paper

--- app/src/main/java/com/thanhng224/app/presentation/screen/ProfileScreen.kt
+++ app/src/main/java/com/thanhng224/app/presentation/screen/ProfileScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
