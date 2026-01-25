a45ce161c2be6771fe299b8f31037af3140c99dd
wagustin <venturasaldanaa@gmail.com>
2026-01-24 23:00:18 -0500

    asdasdafa

--- app/src/main/java/com/thanhng224/app/feature/games/SnakeGameScreen.kt
+++ app/src/main/java/com/thanhng224/app/feature/games/SnakeGameScreen.kt
@@ -28,6 +28,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.font.FontWeight
